c19ed5676646c0d119f057cf69e02bd81912a15a
MohamadAlaskari <m_alaskari96@hotmail.com>
2025-09-21 15:06:04 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">EventHub</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
