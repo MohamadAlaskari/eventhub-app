9f3059324c1aab589cfb13946eb90f015fe87ac5
MohamadAlaskari <m_alaskari96@hotmail.com>
2025-10-05 13:46:14 +0300

    feat (FavoritesService): implement add, remove, check favorite

--- src/services/favorites.service.ts
+++ src/services/favorites.service.ts
@@ -28,7 +28,34 @@ class FavoritesService {
         }
         
     }
+
+    async addFavorite(eventId: string): Promise<void> {
+        try {
+            await httpAuth.post(API_ENDPOINTS.FAVORITE.ADD(eventId));  
+        } catch (error) {
+            throw new Error(`Failed to add Favorite ${error instanceof Error ? error.message : 'Unknown error'}`);
+        }
+    }
+
+    async removeFavorite(eventId: string): Promise<void> {
+        try {
+            await httpAuth.delete(API_ENDPOINTS.FAVORITE.REMOVE(eventId));
+        } catch (error) {
+            throw new Error(`Failed to remove Favorite ${error instanceof Error ? error.message : 'Unknown error'}`);
+        }
+    }
+
+    async checkFavorite(eventId: string): Promise<boolean> {
+        try {
+            const res = await httpAuth.get<boolean>(API_ENDPOINTS.FAVORITE.CHECK(eventId));
+            return res.data;
+        } catch (error) {
+            throw new Error(`Failed to check Favorite ${error instanceof Error ? error.message : 'Unknown error'}`);
+        }
+    }
     
 }
 
 export const favoritesService = new FavoritesService();
+
+// git commit : add favorites service to fetch user favorites and add/remove favorite events
